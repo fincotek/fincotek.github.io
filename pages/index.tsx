23e7b556f6865b1abfec4d2c946fc19e207bc66a
Julien Simonet <julien.simonet@tohero.fr>
2023-03-31 23:01:08 +0200

    style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { Container } from "@mui/material";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
+      <div>
         <h1>Overview</h1>
         <p>
           Fincotek offer wallet analytics CEX & DEX. A token launch platform
